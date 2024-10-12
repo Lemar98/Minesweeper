9e8e1e95657aaf612ed89ad0d7e39dd2b8193ed8
Lemar98 <Martyanov2002@yandex.ru>
2024-10-12 08:45:58 +0300

    now all exceptions extend a main exception

--- src/main/java/ru/lemar98/minesweeper/dto/MinesTooMuchException.java
+++ src/main/java/ru/lemar98/minesweeper/dto/MinesTooMuchException.java
@@ -1,6 +1,8 @@
 package ru.lemar98.minesweeper.dto;
 
-public class MinesTooMuchException extends RuntimeException {
+import ru.lemar98.minesweeper.exceptions.MinesweeperException;
+
+public class MinesTooMuchException extends MinesweeperException {
     public MinesTooMuchException(int max) {
         super("количество мин должно быть не менее 1 и не более %s".formatted(max));
     }
